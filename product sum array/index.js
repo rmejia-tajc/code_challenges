4863a99dfc4de5e3e9433348d461da7d34c7c55c
rmejia-tajc <rmejia.tajc@gmail.com>
2020-01-09 14:28:36 -0700

    add other possible solution

--- product sum array/index.js
+++ product sum array/index.js
@@ -1,6 +1,11 @@
 // Write a function that takes in a "special" array and turns its product sum. A "special" array is a non empty array that contains either integers or other "special" arrays. The product sum of a "special" array is the sum of its elements where "special" arrays inside it should be summed themselves and then multiplied by their level of depth. For example, the product sum of [x,y] is x + y; the product sum of [x, [y, z]] is x + 2y + 2z.
 let sampleArray = [5,2, [7,-1],3, [6, [-13,8],4]]
 //Output: 12
+
+
+
+
+
 function productSum(array, depth) {
   let output = 0;
   for(let i = 0; i < array.length; i++) {
@@ -16,3 +21,21 @@ function productSum(array, depth) {
   return depth * output
 }
 console.log(productSum(sampleArray, 1))
+
+
+
+
+
+function productSum(array, multiplier = 1) {
+    let sum = 0;
+    for(let i = 0; i < array.length; i++) {
+      if(typeof array[i] === "number") {
+        sum += array[i]
+        console.log(sum)
+      } else {
+        sum += productSum(array[i], multiplier + 1)
+      }
+    }
+    return sum * multiplier
+  }
+  console.log(productSum(sampleArray))
