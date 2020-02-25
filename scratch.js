bf4e457f61e67dde156b239d3360b26c0599bb50
rmejia-tajc <rmejia.tajc@gmail.com>
2020-02-24 19:43:42 -0700

    at it again

--- scratch.js
+++ scratch.js
@@ -1,20 +1,55 @@
-var dailyTemperatures = function(T) {
-    let arr = []; // to store the days
-    for(i=0; i<T.length;i++){ // loop though all the temps
-        for(j=i+1;j<T.length;j++){ // check until future temps
-            if(T[i] < T[j]) { //when temp rises, push it and break
-                arr.push(j-i)
-                break;
-            }
-            if(j==T.length-1){ // if it doesn't rise, push 0
-                arr.push(0);
-            }
-        }
+// https://www.hackerrank.com/challenges/kangaroo
+
+// function kangaroo(x1, v1, x2, v2) {
+//     let k1spot = x1;
+//     let k2spot = x2;
+//     let initialDistance = x2 - x1;
+
+//     while (k1spot != k2spot || k1spot < k2spot){
+//         k1spot = k1spot + v1;
+//         k2spot = k2spot + v2;
+//         console.log("k1", k1spot);
+//         console.log("k2", k2spot);
+//         if (k2spot - k1spot > initialDistance){
+//             break
+//         }
+//     }
+//     if (k1spot === k2spot){
+//         return "YES"
+//     } else {
+//         return "NO"
+//     }    
+// }
+
+
+// console.log(kangaroo(0, 3, 4, 2));
+// console.log(kangaroo(0, 2, 5, 3));
+
+
+function kangaroo(x1, v1, x2, v2) {
+    
+    // let k1 = x1 - x1;
+    // console.log("k1", k1);
+    // let k2 = x2 - x1;
+    // console.log("k2", k2);
+
+    if ((x1 - x2) % (v2 - v1) == 0){
+        return "YES"
     }
-    arr.push(0); // for the last temperature, since it will never rise
-    return arr;
+
+    return "NO"    
+    
 }
 
-let T = [73,74,75,71,69,72,76,73];
+console.log(kangaroo(0, 3, 4, 2));
+console.log(kangaroo(0, 2, 5, 3));
+console.log(kangaroo(10, 3, 20, 2)); // 0, 3, 10, 2
+console.log(kangaroo(5, 3, 20, 2));
 
-console.log(dailyTemperatures(T)) // [1,1,4,2,1,1,0,0]
+
+
+// x1 + y * v1 = x2 + y * v2
+
+// (x1 - x2) % (v2 - v1) == 0
+
+// y is number of jumps
